bec2e94d22386105d63fe6e8a755a1fe2e4eea02
Christopher.Bischoff <christopher.bischoff@stitchfix.com>
2019-03-28 13:57:49 -0600

    [update/fibonacci] Travis Update and Scripts
    - still working on Travis Config
    - added/updated scripts for push and deployment

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,7 @@ set -o errexit -o nounset
 export commitID=`git rev-parse HEAD`
 export TRAVIS_REPO_OWNER=${TRAVIS_REPO_SLUG%/*}
 export TRAVIS_REPO_NAME=${TRAVIS_REPO_SLUG#*/}
+aws lambda update-function-code --function-name ${TRAVIS_REPO_NAME} \
+                --s3-bucket ${AWS_S3_BUCKET} \
+                --s3-key ${commitID()}.zip \
+                --region ${AWS_DEFAULT_REGION}
