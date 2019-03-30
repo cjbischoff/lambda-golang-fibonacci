41252d243f559440b15101df520ed7424129a031
Christopher.Bischoff <christopher.bischoff@stitchfix.com>
2019-03-30 09:29:01 -0600

    [update/fibonacci] Travis CI
    - moving push/deploy to travis
    - spec out/echo out the variables to test

--- build.sh
+++ build.sh
@@ -3,7 +3,9 @@
 set -o errexit -o nounset
 
 export commitID=`git rev-parse HEAD`
+echo $commitID
 export TRAVIS_REPO_OWNER=${TRAVIS_REPO_SLUG%/*}
 export TRAVIS_REPO_NAME=${TRAVIS_REPO_SLUG#*/}
-GOOS=linux GOARCH=amd64 go build -o main main.go
-zip ${commitID()}.zip main
+echo $TRAVIS_REPO_NAME
+GOOS=linux GOARCH=amd64 go build -o main lambda_golang_fibonacci.go
+zip ${commitID}.zip main
